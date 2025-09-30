4e2ca74c0591ab4a617c83e0db1c843dc1c1f10d
kotobazza <152398719+kotobazza@users.noreply.github.com>
2025-09-30 14:41:38 +0300

    fix: task with maskified numbers now uses simplified syntax

--- src/Main.java
+++ src/Main.java
@@ -17,14 +17,10 @@ public class Main {
     public static String maskify(String str){
         /*Returns maskified value of string of any length.
         Format: ####1234*/
-        if(str.length() <= 4){
-            return str;
-        }
-        StringBuilder bld = new StringBuilder();
-        for(int i =0; i< str.length()-4; i++) bld.append("#");
+        if(str.length() <= 4) return str;
 
-        bld.append(str.substring(str.length()-4));
-        return bld.toString();
+        return "#".repeat(str.length() - 4) +
+                str.substring(str.length() - 4);
     }
 
     public static void maskifyTest(){
