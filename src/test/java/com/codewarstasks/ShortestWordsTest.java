7388f4791b1677ea02bb2df1985d155852a9ff42
kotobazza <152398719+kotobazza@users.noreply.github.com>
2025-10-02 00:31:06 +0300

    chore: fix mistake in english

--- src/test/java/com/codewarstasks/ShortestWordsTest.java
+++ src/test/java/com/codewarstasks/ShortestWordsTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import static main.java.com.codewarstasks.ShortestWords.findShortestWordLength;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-/// Find shortest word in string and return it's size
+/// Find the shortest word in string and return it's size
 /// There's asserted that string won't be empty
 public class ShortestWordsTest {
     @Test
