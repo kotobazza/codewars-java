0f6addbd43ad64d4d5ece66ba46a9b58a82fa9f8
kotobazza <152398719+kotobazza@users.noreply.github.com>
2025-10-01 14:26:48 +0300

    tests: created test for task with camelcase

---
+++ src/test/java/com/codewarstasks/CamelCaseBreakerTest.java
@@ -0,0 +1,31 @@
+package test.java.com.codewarstasks;
+
+import org.junit.jupiter.api.Test;
+
+import static main.java.com.codewarstasks.CamelCaseBreaker.breakCamelCase;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CamelCaseBreakerTest {
+
+    @Test
+    public void testOneWord(){
+        assertEquals("Insomnia", breakCamelCase("Insomnia"));
+    }
+
+    @Test
+    public void testCamelCaseOne(){
+        assertEquals("camel Case", breakCamelCase("camelCase"));
+
+    }
+
+    @Test
+    public void testCamelCaseTwo(){
+        assertEquals("camel Casing Test", breakCamelCase("camelCasingTest"));
+
+    }
+
+    @Test
+    public void testWithoutCamelCase(){
+        assertEquals("camelcasingtest", breakCamelCase("camelcasingtest"));
+    }
+}
