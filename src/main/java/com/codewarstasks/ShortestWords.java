ffe8c2692c1ced0df7281da2ef291b989d5d0b70
kotobazza <152398719+kotobazza@users.noreply.github.com>
2025-10-02 00:30:24 +0300

    solved: task with shortest words and streams-maps

---
+++ src/main/java/com/codewarstasks/ShortestWords.java
@@ -0,0 +1,9 @@
+package main.java.com.codewarstasks;
+
+import java.util.Arrays;
+
+public class ShortestWords {
+    public static int findShortestWordLength(String line){
+        return Arrays.stream(line.split(" ")).map(String::length).reduce(Integer::min).orElse(Integer.MAX_VALUE);
+    }
+}
---
+++ src/test/java/com/codewarstasks/ShortestWordsTest.java
@@ -0,0 +1,21 @@
+package test.java.com.codewarstasks;
+
+import org.junit.jupiter.api.Test;
+
+import static main.java.com.codewarstasks.ShortestWords.findShortestWordLength;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/// Find shortest word in string and return it's size
+/// There's asserted that string won't be empty
+public class ShortestWordsTest {
+    @Test
+    public void testManyWords1(){
+        assertEquals(3, findShortestWordLength("bitcoin take over the world maybe who knows perhaps"));
+    }
+
+    @Test
+    public void testManyWords2(){
+        assertEquals(3, findShortestWordLength("turns out random test cases are easier than writing out basic ones"));
+
+    }
+}
