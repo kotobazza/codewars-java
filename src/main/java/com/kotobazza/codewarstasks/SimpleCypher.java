0b81cc6ec25bb3a726dd5bd766f80682cf459c5d
kotobazza <152398719+kotobazza@users.noreply.github.com>
2025-10-02 18:45:24 +0300

    solved: task with strange encryption mechanism

---
+++ src/main/java/com/kotobazza/codewarstasks/SimpleCypher.java
@@ -0,0 +1,35 @@
+package main.java.com.kotobazza.codewarstasks;
+
+public class SimpleCypher {
+
+    public static String encrypt(String input){
+        StringBuilder bld = new StringBuilder();
+
+        String[] words = input.split(" ");
+
+        for(int i = 0; i< words.length; i++){
+            String v = words[i];
+            if(!v.isEmpty()){
+                bld.append(v.codePointAt(0));
+            }
+            if(v.length() == 2){
+                bld.append(Character.toChars(v.codePointAt(1)));
+            } else  if (v.length() > 2){
+                bld.append(Character.toChars(v.codePointAt(v.length()-1)));
+                bld.append(v, 2, v.length()-1);
+                bld.append(Character.toChars(v.codePointAt(1)));
+            }
+
+            if(i != words.length-1){
+                bld.append(" ");
+            }
+        }
+        return bld.toString();
+    }
+
+    /// wasn't implemented due to no task on CodeWars for Java
+    public static String decrypt(String input){
+        return "";
+    }
+
+}
---
+++ src/test/java/com/kotobazza/codewarstasks/SimpleCypherTest.java
@@ -0,0 +1,26 @@
+package test.java.com.kotobazza.codewarstasks;
+
+import org.junit.jupiter.api.Test;
+
+import static main.java.com.kotobazza.codewarstasks.SimpleCypher.encrypt;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+/// Simple Cypher
+/// 1. Your message is a string containing space separated words.
+/// 2. The first letter must be converted to its ASCII code.
+/// 3. The second letter must be switched with the last letter
+/// 4. Keepin' it simple: There are no special characters in the input.
+///
+/// Decryption should restore the old string
+/// + Decryption wasn't implemented due to no task on CodeWars for Java
+public class SimpleCypherTest {
+
+    @Test
+    public void testEncryption(){
+        assertEquals("72olle", encrypt("Hello"));
+        assertEquals("104olle 119drlo", encrypt("hello world"));
+        assertEquals("65 119esi 111dl 111lw 108dvei 105n 97n 111ka", encrypt("A wise old owl lived in an oak"));
+        assertEquals("87yh 99na 119e 110to 97ll 98e 108eki 116tah 119esi 111dl 98dri", encrypt("Why can we not all be like that wise old bird"));
+    }
+}
