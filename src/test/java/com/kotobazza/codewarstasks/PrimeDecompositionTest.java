f346d84554bb05418b8489a62389becd6ae85108
kotobazza <152398719+kotobazza@users.noreply.github.com>
2025-10-04 01:12:38 +0300

    chore: cleanup in tests according to maven directory structure

--- src/test/java/com/kotobazza/codewarstasks/PrimeDecompositionTest.java
+++ src/test/java/com/kotobazza/codewarstasks/PrimeDecompositionTest.java
@@ -9,10 +9,10 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class PrimeDecompositionTest {
     @Test
     public void test1(){
-        assertEquals("(2**2)(3**3)(5)(7)(11**2)(17)", NumberedStrings.factors(7775460));
+        assertEquals("(2**2)(3**3)(5)(7)(11**2)(17)", PrimeDecomposition.factors(7775460));
     }
     @Test
     public void test2(){
-        assertEquals("(2**5)(5)(7**2)(11)", NumberedStrings.factors(86240));
+        assertEquals("(2**5)(5)(7**2)(11)", PrimeDecomposition.factors(86240));
     }
 }
